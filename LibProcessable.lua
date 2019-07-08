4cf141b70c91ff2a248d6077c0b60676538907be
Adrian L Lange <p3lim@users.noreply.github.com>
2019-07-08 18:35:07 +0200

    Add 8.2 ores and herbs

--- LibProcessable.lua
+++ LibProcessable.lua
@@ -280,8 +280,8 @@ lib.ores = {
 	[152579] = true, -- Storm Silver Ore
 	[152512] = true, -- Monelite Ore
 	[152513] = true, -- Platinum Ore
-	
 	[155830] = true, -- Runic Core, BfA Jewelcrafting Quest
+	[168185] = true, -- Osmenite Ore
 }
 
 --[[ LibProcessable.herbs
@@ -369,6 +369,7 @@ lib.herbs = {
 	[152506] = true, -- Star Moss
 	[152505] = true, -- Riverbud
 	[152510] = true, -- Anchor Weed
+	[168487] = true, -- Zin'anthid
 }
 
 --[[ LibProcessable.enchantingItems
