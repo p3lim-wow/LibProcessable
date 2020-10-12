36881ae2e9e2589ee1b07dc4d4a8cb817e4d91bc
Adrian L Lange <mail@p3lim.net>
2020-10-12 21:45:34 +0200

    Update version

--- LibProcessable.lua
+++ LibProcessable.lua
@@ -1,4 +1,4 @@
-local MAJOR, MINOR = 'LibProcessable', 42
+local MAJOR, MINOR = 'LibProcessable', 43
 assert(LibStub, MAJOR .. ' requires LibStub')
 
 local lib, oldMinor = LibStub:NewLibrary(MAJOR, MINOR)
