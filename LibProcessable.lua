0c43f97839bdaa7579af28338c688395aea317db
Adrian L Lange <git@p3lim.net>
2022-09-06 21:24:22 +0200

    Merge the data tables and use expansion enums

--- LibProcessable.lua
+++ LibProcessable.lua
@@ -359,166 +359,179 @@ Handler:SetScript('OnEvent', function(self, event, ...)
 	end
 end)
 
---[[ LibProcessable.ores
-Table of all ores that can be prospected by a jewelcrafter.
-
-See [LibProcessable:IsProspectable()](LibProcessable#libprocessableisprospectableitem).
-
-**Notes**:
-* Some items contains a table instead of a boolean
-   * Outland and Pandaria ores have skill level requirements, the tables hold that information
-* This table has different content based on the game version (retail vs classic)
-   * In classic the values represent the required jewelcrafting skill to prospect
---]]
-if CLASSIC then
-	data.ores = {
-		-- https://tbc.wowhead.com/spell=31252/prospecting#comments
-		[2770]  = 1,   -- Copper Ore
-		[2771]  = 50,  -- Tin Ore
-		[2772]  = 125, -- Iron Ore
-		[3858]  = 175, -- Mithril Ore
-		[10620] = 250, -- Thorium Ore
-		[23424] = 275, -- Fel Iron Ore
-		[23425] = 325, -- Adamantite Ore
-	}
-else
-	data.ores = {
-		-- http://www.wowhead.com/spell=31252/prospecting#prospected-from:0+1+17-20
-		[2770] = true, -- Copper Ore
-		[2771] = true, -- Tin Ore
-		[2772] = true, -- Iron Ore
-		[3858] = true, -- Mithril Ore
-		[10620] = true, -- Thorium Ore
-		[23424] = {815, 1}, -- Fel Iron Ore
-		[23425] = {815, 25}, -- Adamantite Ore
-		[36909] = true, -- Cobalt Ore
-		[36910] = true, -- Titanium Ore
-		[36912] = true, -- Saronite Ore
-		[52183] = true, -- Pyrite Ore
-		[52185] = true, -- Elementium Ore
-		[53038] = true, -- Obsidium Ore
-		[72092] = {809, 1}, -- Ghost Iron Ore
-		[72093] = {809, 25}, -- Kyparite
-		[72094] = {809, 75}, -- Black Trillium Ore
-		[72103] = {809, 75}, -- White Trillium Ore
-		[123918] = true, -- Leystone Ore
-		[123919] = true, -- Felslate
-		[151564] = true, -- Empyrium
-		[152579] = true, -- Storm Silver Ore
-		[152512] = true, -- Monelite Ore
-		[152513] = true, -- Platinum Ore
-		[155830] = true, -- Runic Core, BfA Jewelcrafting Quest
-		[168185] = true, -- Osmenite Ore
-		[171828] = true, -- Laestrite
-		[171829] = true, -- Solenium
-		[171830] = true, -- Oxxein
-		[171831] = true, -- Phaedrum
-		[171832] = true, -- Sinvyr
-		[171833] = true, -- Elethium
-		[187700] = true, -- Progenium Ore
-	}
-end
-
---[[ LibProcessable.herbs
-Table of all herbs that can be milled by a scribe.
-
-See [LibProcessable:IsMillable()](LibProcessable#libprocessableismillableitem-ignoremortar).
---]]
-data.herbs = {
-	-- http://www.wowhead.com/spell=51005/milling#milled-from:0+1+17-20
-	[765] = true, -- Silverleaf
-	[785] = true, -- Mageroyal
-	[2447] = true, -- Peacebloom
-	[2449] = true, -- Earthroot
-	[2450] = true, -- Briarthorn
-	[2452] = true, -- Swiftthistle
-	[2453] = true, -- Bruiseweed
-	[3355] = true, -- Wild Steelbloom
-	[3356] = true, -- Kingsblood
-	[3357] = true, -- Liferoot
-	[3358] = true, -- Khadgar's Whisker
-	[3369] = true, -- Grave Moss
-	[3818] = true, -- Fadeleaf
-	[3819] = true, -- Dragon's Teeth
-	[3820] = true, -- Stranglekelp
-	[3821] = true, -- Goldthorn
-	[4625] = true, -- Firebloom
-	[8831] = true, -- Purple Lotus
-	[8836] = true, -- Arthas' Tears
-	[8838] = true, -- Sungrass
-	[8839] = true, -- Blindweed
-	[8845] = true, -- Ghost Mushroom
-	[8846] = true, -- Gromsblood
-	[13463] = true, -- Dreamfoil
-	[13464] = true, -- Golden Sansam
-	[13465] = true, -- Mountain Silversage
-	[13466] = true, -- Sorrowmoss
-	[13467] = true, -- Icecap
-	[22785] = true, -- Felweed
-	[22786] = true, -- Dreaming Glory
-	[22787] = true, -- Ragveil
-	[22789] = true, -- Terocone
-	[22790] = true, -- Ancient Lichen
-	[22791] = true, -- Netherbloom
-	[22792] = true, -- Nightmare Vine
-	[22793] = true, -- Mana Thistle
-	[36901] = true, -- Goldclover
-	[36903] = true, -- Adder's Tongue
-	[36904] = true, -- Tiger Lily
-	[36905] = true, -- Lichbloom
-	[36906] = true, -- Icethorn
-	[36907] = true, -- Talandra's Rose
-	[37921] = true, -- Deadnettle
-	[39969] = true, -- Fire Seed
-	[39970] = true, -- Fire Leaf
-	[52983] = true, -- Cinderbloom
-	[52984] = true, -- Stormvine
-	[52985] = true, -- Azshara's Veil
-	[52986] = true, -- Heartblossom
-	[52987] = true, -- Twilight Jasmine
-	[52988] = true, -- Whiptail
-	[72234] = true, -- Green Tea Leaf
-	[72235] = true, -- Silkweed
-	[72237] = true, -- Rain Poppy
-	[79010] = true, -- Snow Lily
-	[79011] = true, -- Fool's Cap
-	[89639] = true, -- Desecrated Herb
-	[109124] = true, -- Frostweed
-	[109125] = true, -- Fireweed
-	[109126] = true, -- Gorgrond Flytrap
-	[109127] = true, -- Starflower
-	[109128] = true, -- Nagrand Arrowbloom
-	[109129] = true, -- Talador Orchid
-	[124101] = true, -- Aethril
-	[124102] = true, -- Dreamleaf
-	[124103] = true, -- Foxflower
-	[124104] = true, -- Fjarnskaggl
-	[124105] = true, -- Starlight Rose
-	[124106] = true, -- Felwort
-	[128304] = true, -- Yseralline Seed
-	[151565] = true, -- Astral Glory
-	[152511] = true, -- Sea Stalk
-	[152509] = true, -- Siren's Pollen
-	[152508] = true, -- Winter's Kiss
-	[152507] = true, -- Akunda's Bite
-	[152506] = true, -- Star Moss
-	[152505] = true, -- Riverbud
-	[152510] = true, -- Anchor Weed
-	[168487] = true, -- Zin'anthid
-	[168583] = true, -- Widowbloom
-	[168586] = true, -- Rising Glory
-	[168589] = true, -- Marrowroot
-	[169701] = true, -- Deathblossom
-	[170554] = true, -- Vigil's Torch
-	[171315] = true, -- Nightshade
-	[187699] = true, -- First Flower
+data.ores = {
+	-- http://www.wowhead.com/spell=31252/prospecting#prospected-from:0+1+17-20
+	[2770] = 1, -- Copper Ore
+	[2771] = CLASSIC and 50 or 1, -- Tin Ore
+	[2772] = CLASSIC and 125 or 1, -- Iron Ore
+	[3858] = CLASSIC and 175 or 1, -- Mithril Ore
+	[10620] = CLASSIC and 250 or 1, -- Thorium Ore
+	[23424] = CLASSIC and 275 or {LE_EXPANSION_BURNING_CRUSADE, 1}, -- Fel Iron Ore
+	[23425] = CLASSIC and 325 or {LE_EXPANSION_BURNING_CRUSADE, 25}, -- Adamantite Ore
+	[36909] = CLASSIC and 350 or 1, -- Cobalt Ore
+	[36910] = CLASSIC and 450 or 1, -- Titanium Ore
+	[36912] = CLASSIC and 400 or 1, -- Saronite Ore
+	[52183] = 1, -- Pyrite Ore
+	[52185] = 1, -- Elementium Ore
+	[53038] = 1, -- Obsidium Ore
+	[72092] = {LE_EXPANSION_MISTS_OF_PANDARIA, 1}, -- Ghost Iron Ore
+	[72093] = {LE_EXPANSION_MISTS_OF_PANDARIA, 25}, -- Kyparite
+	[72094] = {LE_EXPANSION_MISTS_OF_PANDARIA, 75}, -- Black Trillium Ore
+	[72103] = {LE_EXPANSION_MISTS_OF_PANDARIA, 75}, -- White Trillium Ore
+	[123918] = 1, -- Leystone Ore
+	[123919] = 1, -- Felslate
+	[151564] = 1, -- Empyrium
+	[152579] = 1, -- Storm Silver Ore
+	[152512] = 1, -- Monelite Ore
+	[152513] = 1, -- Platinum Ore
+	[155830] = 1, -- Runic Core, BfA Jewelcrafting Quest
+	[168185] = 1, -- Osmenite Ore
+	[171828] = 1, -- Laestrite
+	[171829] = 1, -- Solenium
+	[171830] = 1, -- Oxxein
+	[171831] = 1, -- Phaedrum
+	[171832] = 1, -- Sinvyr
+	[171833] = 1, -- Elethium
+	[187700] = 1, -- Progenium Ore
 }
 
---[[ LibProcessable.enchantingItems
-Table of special items used in Enchanting quests.
+data.herbs = {
+	-- http://www.wowhead.com/spell=51005/milling#milled-from:0+1+17-20
+	[765] = 1, -- Silverleaf
+	[785] = 1, -- Mageroyal
+	[2447] = 1, -- Peacebloom
+	[2449] = 1, -- Earthroot
+	[2450] = CLASSIC and 25 or 1, -- Briarthorn
+	[2452] = CLASSIC and 25 or 1, -- Swiftthistle
+	[2453] = CLASSIC and 25 or 1, -- Bruiseweed
+	[3355] = CLASSIC and 75 or 1, -- Wild Steelbloom
+	[3356] = CLASSIC and 75 or 1, -- Kingsblood
+	[3357] = CLASSIC and 75 or 1, -- Liferoot
+	[3358] = CLASSIC and 125 or 1, -- Khadgar's Whisker
+	[3369] = CLASSIC and 75 or 1, -- Grave Moss
+	[3818] = CLASSIC and 125 or 1, -- Fadeleaf
+	[3819] = CLASSIC and 125 or 1, -- Dragon's Teeth
+	[3820] = CLASSIC and 25 or 1, -- Stranglekelp
+	[3821] = CLASSIC and 125 or 1, -- Goldthorn
+	[4625] = CLASSIC and 175 or 1, -- Firebloom
+	[8831] = CLASSIC and 175 or 1, -- Purple Lotus
+	[8836] = CLASSIC and 175 or 1, -- Arthas' Tears
+	[8838] = CLASSIC and 175 or 1, -- Sungrass
+	[8839] = CLASSIC and 175 or 1, -- Blindweed
+	[8845] = CLASSIC and 175 or 1, -- Ghost Mushroom
+	[8846] = CLASSIC and 175 or 1, -- Gromsblood
+	[13463] = CLASSIC and 225 or 1, -- Dreamfoil
+	[13464] = CLASSIC and 225 or 1, -- Golden Sansam
+	[13465] = CLASSIC and 225 or 1, -- Mountain Silversage
+	[13466] = CLASSIC and 225 or 1, -- Sorrowmoss
+	[13467] = CLASSIC and 200 or 1, -- Icecap
+	[22785] = CLASSIC and 275 or 1, -- Felweed
+	[22786] = CLASSIC and 275 or 1, -- Dreaming Glory
+	[22787] = CLASSIC and 275 or 1, -- Ragveil
+	[22789] = CLASSIC and 275 or 1, -- Terocone
+	[22790] = CLASSIC and 275 or 1, -- Ancient Lichen
+	[22791] = CLASSIC and 275 or 1, -- Netherbloom
+	[22792] = CLASSIC and 275 or 1, -- Nightmare Vine
+	[22793] = CLASSIC and 275 or 1, -- Mana Thistle
+	[36901] = CLASSIC and 325 or 1, -- Goldclover
+	[36903] = CLASSIC and 325 or 1, -- Adder's Tongue
+	[36904] = CLASSIC and 325 or 1, -- Tiger Lily
+	[36905] = CLASSIC and 325 or 1, -- Lichbloom
+	[36906] = CLASSIC and 325 or 1, -- Icethorn
+	[36907] = CLASSIC and 325 or 1, -- Talandra's Rose
+	[37921] = CLASSIC and 325 or 1, -- Deadnettle
+	[39970] = CLASSIC and 325 or 1, -- Fire Leaf
+	-- [39969] = CLASSIC and ? or nil, -- Fire Seed
+	[52983] = 1, -- Cinderbloom
+	[52984] = 1, -- Stormvine
+	[52985] = 1, -- Azshara's Veil
+	[52986] = 1, -- Heartblossom
+	[52987] = 1, -- Twilight Jasmine
+	[52988] = 1, -- Whiptail
+	[72234] = 1, -- Green Tea Leaf
+	[72235] = 1, -- Silkweed
+	[72237] = 1, -- Rain Poppy
+	[79010] = 1, -- Snow Lily
+	[79011] = 1, -- Fool's Cap
+	[89639] = 1, -- Desecrated Herb
+	[109124] = 1, -- Frostweed
+	[109125] = 1, -- Fireweed
+	[109126] = 1, -- Gorgrond Flytrap
+	[109127] = 1, -- Starflower
+	[109128] = 1, -- Nagrand Arrowbloom
+	[109129] = 1, -- Talador Orchid
+	[124101] = 1, -- Aethril
+	[124102] = 1, -- Dreamleaf
+	[124103] = 1, -- Foxflower
+	[124104] = 1, -- Fjarnskaggl
+	[124105] = 1, -- Starlight Rose
+	[124106] = 1, -- Felwort
+	[128304] = 1, -- Yseralline Seed
+	[151565] = 1, -- Astral Glory
+	[152511] = 1, -- Sea Stalk
+	[152509] = 1, -- Siren's Pollen
+	[152508] = 1, -- Winter's Kiss
+	[152507] = 1, -- Akunda's Bite
+	[152506] = 1, -- Star Moss
+	[152505] = 1, -- Riverbud
+	[152510] = 1, -- Anchor Weed
+	[168487] = 1, -- Zin'anthid
+	[168583] = 1, -- Widowbloom
+	[168586] = 1, -- Rising Glory
+	[168589] = 1, -- Marrowroot
+	[169701] = 1, -- Deathblossom
+	[170554] = 1, -- Vigil's Torch
+	[171315] = 1, -- Nightshade
+	[187699] = 1, -- First Flower
+}
 
-See [LibProcessable:IsDisenchantable()](LibProcessable#libprocessableisdisenchantableitem).
---]]
+data.containers = {
+	-- https://www.wowhead.com/items?filter=10:195;1:2;:0
+	[7209]   = 1, -- Tazan's Satchel
+	[4632]   = CLASSIC and 1 or 15, -- Ornate Bronze Lockbox
+	[6712]   = CLASSIC and 1 or nil, -- Practice Lock
+	[4633]   = CLASSIC and 25 or 15, -- Heavy Bronze Lockbox
+	[4634]   = CLASSIC and 70 or 15, -- Iron Lockbox
+	[5046]   = CLASSIC and 70 or nil, -- Locked Gift (removed in TBC)
+	[4636]   = CLASSIC and 125 or 15, -- Strong Iron Lockbox
+	[4637]   = CLASSIC and 175 or 15, -- Steel Lockbox
+	[4638]   = CLASSIC and 225 or 15, -- Reinforced Steel Lockbox
+	[5758]   = CLASSIC and 225 or 15, -- Mithril Lockbox
+	[5759]   = CLASSIC and 225 or 15, -- Thorium Lockbox
+	[5760]   = CLASSIC and 225 or 15, -- Eternium Lockbox
+	[6354]   = CLASSIC and 1 or 15, -- Small Locked Chest
+	[6355]   = CLASSIC and 70 or 15, -- Sturdy Locked Chest
+	[7869]   = CLASSIC and 70 or nil,   -- Lucius's Lockbox
+	[12033]  = CLASSIC and 275 or 15, -- Thaurissan Family Jewels
+	[13875]  = CLASSIC and 175 or 15, -- Ironbound Locked Chest
+	[13918]  = CLASSIC and 250 or 15, -- Reinforced Locked Chest
+	[16882]  = CLASSIC and 1 or 15, -- Battered Junkbox
+	[16883]  = CLASSIC and 70 or 15, -- Worn Junkbox
+	[16884]  = CLASSIC and 175 or 15, -- Sturdy Junkbox
+	[16885]  = CLASSIC and 250 or 15, -- Heavy Junkbox
+	[106895] = 15, -- Iron-Bound Junkbox
+	[29569]  = CLASSIC and 300 or 30, -- Strong Junkbox
+	[31952]  = CLASSIC and 325 or 30, -- Khorium Lockbox
+	[43575]  = CLASSIC and 350 or 30, -- Reinforced Junkbox
+	[43622]  = CLASSIC and 375 or 30, -- Froststeel Lockbox
+	[43624]  = CLASSIC and 400 or 30, -- Titanium Lockbox
+	[45986]  = CLASSIC and 400 or 30, -- Tiny Titanium Lockbox
+	[63349]  = 30, -- Flame-Scarred Junkbox
+	[68729]  = 30, -- Elementium Lockbox
+	[88165]  = 35, -- Vine-Cracked Junkbox
+	[88567]  = 35, -- Ghost Iron Lockbox
+	[116920] = 40, -- True Steel Lockbox
+	[121331] = 45, -- Leystone Lockbox
+	[169475] = 50, -- Barnacled Lockbox
+	[179311] = 60, -- Synvir Lockbox
+	[180522] = 60, -- Phaedrum Lockbox
+	[180532] = 60, -- Oxxein Lockbox
+	[180533] = 60, -- Solenium Lockbox
+	[186161] = 60, -- Stygian Lockbox     TODO: confirm level requirement
+	[186160] = 60, -- Locked Artifact Case
+	[188787] = 60, -- Locked Broker Luggage
+}
+
 data.enchantingItems = {
 	-- Legion enchanting quest line
 	[137195] = true, -- Highmountain Armor
@@ -531,92 +544,6 @@ data.enchantingItems = {
 	[182067] = true, -- Antique Duelist's Rapier
 	[181991] = true, -- Antique Stalker's Bow
 }
-
---[[ LibProcessable.containers
-Table of all items that can be opened with a Rogue's _Pick Lock_ ability, or with profession keys.
-
-The value is the required skill to open the item.
-
-See [LibProcessable:IsOpenable()](LibProcessable#libprocessableisopenableitem) and
-[LibProcessable:IsOpenableProfession()](LibProcessable#libprocessableisopenableprofessionitem).
-
-**Notes**:
-* This table has different content based on the game version (retail vs classic).
---]]
-if(CLASSIC) then
-	data.containers = {
-		-- https://classic.wowhead.com/items?filter=10:195;1:2;:0
-		[4632]  = 1,    -- Ornate Bronze Lockbox
-		[6354]  = 1,    -- Small Locked Chest
-		[6712]  = 1,    -- Practice Lock
-		[7209]  = 1,    -- Tazan's Satchel
-		[16882] = 1,    -- Battered Junkbox
-		[4633]  = 25,   -- Heavy Bronze Lockbox
-		[4634]  = 70,   -- Iron Lockbox
-		[5046]  = 70,   -- Locked Gift
-		[6355]  = 70,   -- Sturdy Locked Chest
-		[7869]  = 70,   -- Lucius's Lockbox
-		[16883] = 70,   -- Worn Junkbox
-		[4636]  = 125,  -- Strong Iron Lockbox
-		[4637]  = 175,  -- Steel Lockbox
-		[13875] = 175,  -- Ironbound Locked Chest
-		[16884] = 175,  -- Sturdy Junkbox
-		[4638]  = 225,  -- Reinforced Steel Lockbox
-		[5758]  = 225,  -- Mithril Lockbox
-		[5759]  = 225,  -- Thorium Lockbox
-		[5760]  = 225,  -- Eternium Lockbox
-		[13918] = 250,  -- Reinforced Locked Chest
-		[16885] = 250,  -- Heavy Junkbox
-		[12033] = 275,  -- Thaurissan Family Jewels
-		[24282] = 5000, -- Rogue's Diary (lockpick requirement sourced from comments, unverified)
-		[29569] = 300,  -- Strong Junkbox
-		[31952] = 325,  -- Khorium Lockbox
-	}
-else
-	data.containers = {
-		-- https://www.wowhead.com/items?filter=10:195;1:2;:0
-		[7209]   = 0,  -- Tazan's Satchel
-		[4632]   = 15, -- Ornate Bronze Lockbox
-		[4633]   = 15, -- Heavy Bronze Lockbox
-		[4634]   = 15, -- Iron Lockbox
-		[4636]   = 15, -- Strong Iron Lockbox
-		[4637]   = 15, -- Steel Lockbox
-		[4638]   = 15, -- Reinforced Steel Lockbox
-		[5758]   = 15, -- Mithril Lockbox
-		[5759]   = 15, -- Thorium Lockbox
-		[5760]   = 15, -- Eternium Lockbox
-		[6354]   = 15, -- Small Locked Chest
-		[6355]   = 15, -- Sturdy Locked Chest
-		[12033]  = 15, -- Thaurissan Family Jewels
-		[13875]  = 15, -- Ironbound Locked Chest
-		[13918]  = 15, -- Reinforced Locked Chest
-		[16882]  = 15, -- Battered Junkbox
-		[16883]  = 15, -- Worn Junkbox
-		[16884]  = 15, -- Sturdy Junkbox
-		[16885]  = 15, -- Heavy Junkbox
-		[106895] = 15, -- Iron-Bound Junkbox
-		[29569]  = 30, -- Strong Junkbox
-		[31952]  = 30, -- Khorium Lockbox
-		[43575]  = 30, -- Reinforced Junkbox
-		[43622]  = 30, -- Froststeel Lockbox
-		[43624]  = 30, -- Titanium Lockbox
-		[45986]  = 30, -- Tiny Titanium Lockbox
-		[63349]  = 30, -- Flame-Scarred Junkbox
-		[68729]  = 30, -- Elementium Lockbox
-		[88165]  = 35, -- Vine-Cracked Junkbox
-		[88567]  = 35, -- Ghost Iron Lockbox
-		[116920] = 40, -- True Steel Lockbox
-		[121331] = 45, -- Leystone Lockbox
-		[169475] = 50, -- Barnacled Lockbox
-		[179311] = 50, -- Synvir Lockbox
-		[180522] = 50, -- Phaedrum Lockbox
-		[180532] = 50, -- Oxxein Lockbox
-		[180533] = 50, -- Solenium Lockbox
-		[186161] = 50, -- Stygian Lockbox
-		[186160] = 50, -- Locked Artifact Case
-		[188787] = 50, -- Locked Broker Luggage
-	}
-end
 
 data.professionCategories = {
 	[171] = { -- Alchemy
